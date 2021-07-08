36c1cc5b7b0dd9a97f841fc4f95b7fdac71494c9
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-07-08 09:42:54 -0600

    Add project files.

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+
+        }
+        public void MakeDeposit(decimal amount, DateTime date, string note )
+        {
+
+        }
+        public void WithDrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Kendra", 10000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} amount.");
+        }
+    }
+}
