514ed084351307b547c77b401f208edeffdf9918
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2021-07-08 13:30:50 -0600

    Added deposit and withdrawal capabilities.

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -10,21 +10,48 @@ namespace MySuperBank
     {
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance { 
+            get{
+               decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                   balance += item.Amount;
+                }
+                return balance;
+            }}
+        private static int accountNumberSeed = 1234567890;
 
+        private List<Transaction> allTransactions = new List<Transaction>();
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
-            this.Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
 
         }
-        public void MakeDeposit(decimal amount, DateTime date, string note )
+        public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
-        public void WithDrawal(decimal amount, DateTime date, string note)
-        {
 
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -8,6 +8,23 @@ namespace MySuperBank
         {
             var account = new BankAccount("Kendra", 10000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} amount.");
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(-300, DateTime.Now, "Stealing");
+  
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                return;
+            }
+            account.MakeWithdrawal(50, DateTime.Now, "X-Box game");
+            Console.WriteLine(account.Balance);
         }
+
     }
 }
